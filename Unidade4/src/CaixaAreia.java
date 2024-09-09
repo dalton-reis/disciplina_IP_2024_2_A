594ad2c00290ef9b0ace4b04b573cc73349249f6
dalton-reis <dalton.reis@gmail.com>
2024-09-09 19:54:18 -0300

    .

--- Unidade4/src/CaixaAreia.java
+++ Unidade4/src/CaixaAreia.java
@@ -1,19 +1,53 @@
-import java.util.Scanner;
+//import java.util.Scanner;
+
+// 2 valores quem é menor é maior
 
 public class CaixaAreia {
 	public static void main(String[] args) {
+		// Scanner teclado = new Scanner(System.in);
+		int valorA = 15;		//maior
+		int valorB = 6;  		//menor
+		int valorC = 9;			//medio
+		int valorMenor = 0;
+		int valorMedio = 0;
+		int valorMaior = 0;
 
-		Scanner teclado = new Scanner(System.in);
+		// Testar se o valorA é menor ...
+		if (valorA < valorB && valorA < valorC) { // A B C
+			valorMenor = valorA;
+			if (valorB < valorC) { // B C (medio maior)
+				valorMedio = valorB;
+				valorMaior = valorC;
+			}
+			else {
+				valorMedio = valorC;
+				valorMaior = valorB;
+			}	
+		}
+		if (valorB < valorA && valorB < valorC) { // B A C
+			valorMenor = valorB;
+			if (valorA < valorC) { // A C (medio maior)
+				valorMedio = valorA;
+				valorMaior = valorC;
+			}
+			else {
+				valorMedio = valorC;
+				valorMaior = valorA;
+			}	
+		}
+		if (valorC < valorA && valorC < valorB) { // C A B 
+			valorMenor = valorC;
+			if (valorA < valorB) { // A B (medio maior)
+				valorMedio = valorA;
+				valorMaior = valorB;
+			}
+			else {
+				valorMedio = valorB;
+				valorMaior = valorA;
+			}	
+		}
+		System.out.println("Menor: " + valorMenor + " - Medior: " + valorMedio+ " - Maior: " + valorMaior);
 
-		int numero = 10;
-
-		numero = numero / 200;
-		numero /= 200;
-		numero++;
-		System.out.println(numero);
-
-
-		teclado.close();
-
+		// teclado.close();
 	}
 }
