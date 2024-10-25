ee891b20956ebd34bdfa653d9042cbac2bc61ad9
dalton-reis <dalton.reis@gmail.com>
2024-10-24 21:46:26 -0300

    .

--- Unidade6/src/Uni6Exe01.java
+++ Unidade6/src/Uni6Exe01.java
@@ -5,23 +5,30 @@ public class Uni6Exe01 {
     Scanner teclado = new Scanner(System.in);
     
     // criei o vetor
-    final int valorTamanho = 3;
+    final int valorTamanho = 10;
     int valor[] = new int[valorTamanho];
 
     // ler vetor
+    valorLer(valorTamanho, valor, teclado);
+ 
+    // escrever vetor
+    valorEscrever(valorTamanho, valor);
+    
+    teclado.close();
+  }
+
+  private void valorLer(int valorTamanho, int valor[], Scanner teclado) {
     for (int i = 0; i < valorTamanho; i++) {
       System.out.print("Valor[" + i + "]: ");
       valor[i] = teclado.nextInt();
     }
-    System.out.println("----");
-    // escrever vetor
+  }
+
+  private void valorEscrever(int valorTamanho, int valor[]) {
     for (int i = valorTamanho-1; i >= 0; i--) {
       System.out.println("Valor[" + i + "]: " + valor[i]);
     }
-
-    teclado.close();
   }
-
 
   public static void main(String[] args) {
     new Uni6Exe01();
