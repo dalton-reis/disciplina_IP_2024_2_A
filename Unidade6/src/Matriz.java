fc58966ccdb799b44301d4376b13de6efe52627a
dalton-reis <dalton.reis@gmail.com>
2024-11-07 19:50:23 -0300

    .

---
+++ Unidade6/src/Matriz.java
@@ -0,0 +1,19 @@
+public class Matriz {
+  private Matriz() {
+    int mat[][] = { {34,21},{3,8},{55,6}};
+
+    int maiorValor = mat[0][0];
+    for (int lin = 0; lin < 3; lin++) {
+      for (int col = 0; col < 2; col++) {
+        if (mat[lin][col] > maiorValor) {
+          maiorValor = mat[lin][col];
+        }
+      }
+    }
+    System.out.println(maiorValor);
+  }
+
+  public static void main(String[] args) {
+    new Matriz();
+  }
+}
