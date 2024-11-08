de438c21c7a7849bc943cbb17c960b4a03507230
dalton-reis <dalton.reis@gmail.com>
2024-11-07 21:15:56 -0300

    .

---
+++ Unidade6/src/Uni6Exe10.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Uni6Exe10 {
+  private Uni6Exe10() {
+    Scanner teclado = new Scanner(System.in);
+    int vet[] = new int[5];  ///[FIXME:] CUIDADO é 50
+    int posFinal = 0;
+
+    int opcao;
+    do {
+      System.out.print("Opção: ");
+      opcao = teclado.nextInt();
+      // Menu
+      switch (opcao) {
+        case 1: // 1 Incluir
+          posFinal = vetIncluir_1(vet, teclado, posFinal);
+
+          break;
+        case 2: // 2 Pesquisa
+
+          break;
+        case 3: // 3 Alterar
+          break;
+        case 4: // 4 Excluir
+          break;
+        case 5: // 5 Mostrar
+          vetMostrar_5(vet, posFinal);
+          break;
+        case 6: // 6 Ordenar
+          break;
+        case 7: // 7 Inverter
+          break;
+        case 8: // 8 Sair
+          System.out.println("Sair do sistema!");
+          break;
+        default:
+          System.out.println("Opçao ERRADA!!");
+          break;
+      }
+
+    } while (opcao != 8); // Fim
+
+    teclado.close();
+  }
+
+  private int vetIncluir_1(int vet[], Scanner teclado, int posFinal) {
+    System.out.print("Ler valor: ");
+    int valor = teclado.nextInt();
+    vet[posFinal] = valor;
+    posFinal+=1;
+    return posFinal;
+  }
+
+  private void vetPesquisa_2() {
+  }
+
+  private void vetAlterar_3() {
+  }
+
+  private void vetExcluir_4() {
+  }
+
+  private void vetMostrar_5(int vet[], int posFinal) {
+    for (int i = 0; i < posFinal; i++) {
+      System.out.println("vetor[" + i + "]: " + vet[i]);
+    }
+  }
+
+  private void vetOrdenar_6() {
+  }
+
+  private void vetInverter_7() {
+  }
+
+  public static void main(String[] args) {
+    new Uni6Exe10();
+  }
+}
