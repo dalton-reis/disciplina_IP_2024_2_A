aad2e60867367b309e4ca506d51942d702e1cb57
dalton-reis <dalton.reis@gmail.com>
2024-11-11 20:02:11 -0300

    .

--- Unidade6/src/VetorExcluir.java
+++ Unidade6/src/VetorExcluir.java
@@ -1,38 +1,35 @@
 public class VetorExcluir {
   private VetorExcluir() {
     int vetor[] = new int[5];
-    vetor[0] = 10; 
-    vetor[1] = 20; 
-    vetor[2] = 30; 
+    vetor[0] = 10;
+    vetor[1] = 20;
+    vetor[2] = 30;
     vetor[3] = 40;
     vetor[4] = 50;
     int vetPos = 4;
-    
-    vetorImprimir(vetor,vetPos);
-    
+
+    vetorImprimir(vetor, vetPos);
+
     int vetPosExc = 4;
-    vetPos = vetorExcluir(vetor,vetPos,vetPosExc);
-    
-    vetorImprimir(vetor,vetPos);
+    vetPos = vetorExcluir(vetor, vetPos, vetPosExc);
+
+    vetorImprimir(vetor, vetPos);
   }
 
-  
   private int vetorExcluir(int vetor[], int vetPos, int vetPosExc) {
-    if (vetPosExc == vetPos) {
-      vetPos--;
-    } else {
+    if (vetPosExc != vetPos) {
       for (int i = vetPosExc; i <= vetPos; i++) {
-        vetor[i] = vetor[i+1];
-      }      
-      vetPos--;
+        vetor[i] = vetor[i + 1];
+      }
     }
+    vetPos--;
     return vetPos;
   }
 
   private void vetorImprimir(int vetor[], int vetPos) {
     for (int i = 0; i <= vetPos; i++) {
       System.out.println("Vetor[" + i + "]: " + vetor[i]);
-      
+
     }
 
   }
